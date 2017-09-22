d9c1108cf80bbbd76be4025e6a422c73d76d04ac
Stephen Cheng <aw3someone@gmail.com>
2017-09-22 10:08:50 -0700

    Hypergeometric, Cumulative Hypergeometric, Multivariate Hypergeometric

--- HyperGeometricApp/Program.cs
+++ HyperGeometricApp/Program.cs
@@ -12,15 +12,85 @@ namespace HyperGeometricApp
         public static BigInteger[] factLUT { get; set; }
         static void Main(string[] args)
         {
+            BigInteger handSize = 3;
             factLUT = new BigInteger[61];
             factLUT[0] = 1;
             for (int i = 1; i < factLUT.Length; i++)
             {
                 factLUT[i] = i * factLUT[i - 1];
             }
-            Deck d = new Deck();
-            d.AddCategory(new Category("Brilliant Fusion", 1, 3));
-            d.AddCategory(new Category("Other Cards", 0, 37));
+            Deck deck = new Deck();
+            deck.AddCategory(new Category("Brilliant Fusion", 3, 1));
+            deck.AddCategory(new Category("Other Cards", 37, 0));
+
+            Deck success = new Deck();
+            Deck other = new Deck();
+
+            foreach (Category c in deck.Categories)
+            {
+                if (c.desired > 0)
+                {
+                    success.AddCategory(c);
+                }
+                else
+                {
+                    other.AddCategory(c);
+                }
+            }
+            if (success.GetCardCount() > handSize)
+            {
+                Console.WriteLine("Impossible result: total successes greater than hand size");
+            }
+            double r = (double)(BiDist(3, 1) * BiDist(37, 4)) / (double) BiDist(40, 5);
+            r += (double)(BiDist(3, 2) * BiDist(37, 3)) / (double) BiDist(40, 5);
+            r += (double)(BiDist(3, 3) * BiDist(37, 2)) / (double) BiDist(40, 5);
+            Console.WriteLine(r);
+            Console.WriteLine(CumulHyGeo(40, 5, 3, 1));
+            Console.WriteLine(MultHyGeo(40, 5, new int[]{ 3 }, new int[] { 1 }));
+        }
+
+        /**
+         * Hypergeometric Distribution
+         * N = population, n = sample size, K = successes in population, k = successes in sample
+         */
+        public static double HyGeo(int N, int n, int K, int k)
+        {
+            return (double) (BiDist(K, k) * BiDist(N - K, n - k)) / (double) BiDist(N, n);
+        }
+
+        
+        // Cumulative Hypergeometric Distribution
+        public static double CumulHyGeo(int N, int n, int K, int k)
+        {
+            double r = 0;
+            for (int i = k; i <= K; i++)
+            {
+                r += HyGeo(N, n, K, i);
+            }
+            return r;
+        }
+
+        // Multivariate Hypergeometric Distribution
+        public static double MultHyGeo(int N, int n, int[] K, int[] k)
+        {
+            double r = (double) BiDist(N - K.Sum(), n - k.Sum()) / (double) BiDist(N, n);
+            for (int i = 0; i < K.Length; i++)
+            {
+                r *= (double) BiDist(K[i], k[i]);
+            }
+            return r;
+        }
+
+        // Cumulative Multivariate Hypergeometric Distribution
+        public static double CumulMultHyGeo(int N, int n, int[] K, int[] k)
+        {
+            Console.WriteLine("KILL ME");
+            return 0;
+        }
+
+        public static BigInteger BiDist(int n, int k)
+        {
+            return factLUT[n] / (factLUT[k] * factLUT[n - k]);
         }
     }
 
@@ -44,18 +114,32 @@ namespace HyperGeometricApp
             }
             return count;
         }
+        public override string ToString()
+        {
+            string str = "";
+            foreach (Category c in Categories)
+            {
+                str += $"{c}\n";
+            }
+            str += $"Card Count: {GetCardCount()}";
+            return str;
+        }
     }
 
     class Category
     {
-        public String CategoryName { get; set; }
+        public string CategoryName { get; set; }
         public int desired { get; set; }
         public int count { get; set; }
-        public Category(String cat, int d, int c)
+        public Category(string cat, int c, int d)
         {
             CategoryName = cat;
-            desired = d;
             count = c;
+            desired = d;
+        }
+        public override string ToString()
+        {
+            return $"{CategoryName}, {count}, {desired}";
         }
     }
 
